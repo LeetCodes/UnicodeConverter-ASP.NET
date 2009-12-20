d2554eee85ea98bab241b67a5b915d419375bed2
MohammadNezhad <MohammadNezhad@localhost>
2009-12-20 10:03:21 +0000

    WorkItem #11267 - Adding empty base class for web-based module, pages and master pages

---
+++ UnicodeConverter/trunk/Framework/PageBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hajloo.FOSS.UnicodeConverter.Framework
+{
+    public class PageBase : System.Web.UI.Page
+    {
+    }
+}
