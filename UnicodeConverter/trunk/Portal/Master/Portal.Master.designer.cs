c8eff65125977f851bbb3f6d06f9e4f66cc45f99
MohammadNezhad <MohammadNezhad@localhost>
2010-01-04 09:14:56 +0000

    workItem #11105 - Editing Master Page and enabling MasterPageMainContentPlaceHolder

--- UnicodeConverter/trunk/Portal/Master/Portal.Master.designer.cs
+++ UnicodeConverter/trunk/Portal/Master/Portal.Master.designer.cs
@@ -41,111 +41,12 @@ namespace Hajloo.FOSS.UnicodeConverter.Web.UI.Master {
         protected global::System.Web.UI.ScriptManager UnicodeConverterScriptManager;
         
         /// <summary>
-        /// lnbConvertToUnicode control.
+        /// UnicodeConverterContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnbConvertToUnicode;
-        
-        /// <summary>
-        /// lnbConvertFromUnicode control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnbConvertFromUnicode;
-        
-        /// <summary>
-        /// lnbUnicodeInformation control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnbUnicodeInformation;
-        
-        /// <summary>
-        /// lnbStandards control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnbStandards;
-        
-        /// <summary>
-        /// lnbHelpAndHowTo control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnbHelpAndHowTo;
-        
-        /// <summary>
-        /// lnbAbout control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lnbAbout;
-        
-        /// <summary>
-        /// AboutControl control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Hajloo.FOSS.UnicodeConverter.Web.UI.Modules.About AboutControl;
-        
-        /// <summary>
-        /// ConvertFromUnicode control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Hajloo.FOSS.UnicodeConverter.Web.UI.Modules.ConvertFromUnicode ConvertFromUnicode;
-        
-        /// <summary>
-        /// ConvertToUnicode control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Hajloo.FOSS.UnicodeConverter.Web.UI.Modules.ConvertToUnicode ConvertToUnicode;
-        
-        /// <summary>
-        /// StandardsControl control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Hajloo.FOSS.UnicodeConverter.Web.UI.Modules.Standards StandardsControl;
-        
-        /// <summary>
-        /// UnicodeInformationControl control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Hajloo.FOSS.UnicodeConverter.Web.UI.Modules.UnicodeInformation UnicodeInformationControl;
-        
-        /// <summary>
-        /// HelpAndHowToControl control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Hajloo.FOSS.UnicodeConverter.Web.UI.Modules.HelpAndHowTo HelpAndHowToControl;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder UnicodeConverterContent;
     }
 }
